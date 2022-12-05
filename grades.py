ffa59e29350e17f874bb4ce2c13f478709b142ab
Johanna Liu <johanna.liu@mths.ca>
2022-12-05 23:06:49 +0000

    fixed

--- grades.py
+++ grades.py
@@ -12,49 +12,36 @@ def calculate_grade(grade: str) -> int:
     match grade:
         case "4+":
             percent = 97
-            return percent
         case "4":
             percent = 87
-            return percent
         case "4-":
             percent = 80
-            return percent
         case "3+":
             percent = 78
-            return percent
         case "3":
             percent = 75
-            return percent
         case "3-":
             percent = 70
-            return percent
         case "2+":
             percent = 68
-            return percent
         case "2":
             percent = 65
-            return percent
         case "2-":
             percent = 61
-            return percent
         case "1+":
             percent = 58
-            return percent
         case "1":
             percent = 54
-            return percent
         case "1-":
             percent = 51
-            return percent
         case "R":
             percent = 30
-            return percent
         case "NE":
             percent = 0
-            return percent
         case _:
             percent = -1
-            return percent
+
+    return percent
 
 
 def main():
